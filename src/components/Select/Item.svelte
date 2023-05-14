b7040d18036d0d1b513dbb89e6d8bba1f58fc2d7
Blookers <blookyl305@gmail.com>
2023-05-14 17:55:48 +1000

    fix select indicator styling

--- src/components/Select/Item.svelte
+++ src/components/Select/Item.svelte
@@ -11,7 +11,7 @@
 <button type="button" class="focusable" bind:this={items[value]} class:selected on:click|stopPropagation={() => set(value, items[value].childNodes)}>
 	<slot/>
 	{#if selected}
-		<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+		<svg class="indicator" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
 			<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
 		</svg>
 	{/if}
